7116beb4aa45befd4099ecc5c31b6bbc2ca8c221
Derek <derek.yao.graham99@gmail.com>
2026-09-13 17:32:24 +1000

    Refactor homework.py to remove 'banana' from cart
    Removed comments and added a loop to remove all 'banana' items from the shopping cart.

--- homework.py
+++ homework.py
@@ -1,23 +1,19 @@
-# In class, we did this exercise that adds a bunch of items to mom's shopping cart, then we counted the occurrence of each grocery item in the shopping cart.
-# Find the TODO section of this file, and find instructions there.
-
 import random
+
 
 def count_occurrence(item, my_list):
     total = 0
     for i in my_list:
         if i == item:
-            total += 1 # total = total + 1
+            total += 1
     return total
 
-# mom bought a lot of grocery items
+
 grocery_item = ["apple", "banana", "broccoli", "milk", "bread"]
 shopping_cart = []
 
 for i in range(100):
-    # throws random items from grocery_item into shopping_cart
     shopping_cart.append(random.choice(grocery_item))
-
 
 print("Mom bought:")
 print(str(count_occurrence("apple", shopping_cart)) + " apples")
@@ -26,21 +22,13 @@ print(str(count_occurrence("broccoli", shopping_cart)) + " broccolis")
 print(str(count_occurrence("milk", shopping_cart)) + " gallons of milk")
 print(str(count_occurrence("bread", shopping_cart)) + " loaves of bread")
 
+for i in reversed (range(len(shopping_cart))):
+    if shopping_cart[i] == "banana":
+        shopping_cart.pop(i)
 
-# TODO: write a for loop that removes all "banana"s from shopping_cart
-for i in ...
-    ...
-    
 print("After removing all bananas, mom bought:")
 print(str(count_occurrence("apple", shopping_cart)) + " apples")
 print(str(count_occurrence("banana", shopping_cart)) + " bananas")
 print(str(count_occurrence("broccoli", shopping_cart)) + " broccolis")
 print(str(count_occurrence("milk", shopping_cart)) + " gallons of milk")
 print(str(count_occurrence("bread", shopping_cart)) + " loaves of bread")
-
-
-
-
-
-
-
